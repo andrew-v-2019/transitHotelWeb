0c9ebaaeb98c286b230731531033af593517eff6
Andrew <andrew@sparkequation.com>
2018-09-11 01:26:25 +0700

    Photo gallery

--- files/scripts.js
+++ files/scripts.js
@@ -1,10 +1,27 @@
 
 
-  $(function(){
-     
-    $('#myCarousel').carousel(
-      {
-        cycle:true, 
-        interval:2000
-      });
+$(function () {
+  $('#myCarousel').carousel(
+    {
+      cycle: true,
+      interval: 1500
+    });
+  $("[data-fancybox]").fancybox({ loop: true });
+  $.fancybox.defaults.animationEffect = "zoom-in-out";
+
+  $("#nav").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 500);
   });
+
+  $("#toTop").on("click", function (event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 200);
+  });
+
+
+});
